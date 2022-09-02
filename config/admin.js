2f490b28291cb02b192607fdfc970d3e16f2d1b9
Alex Robert <div@alexrobert.com>
2022-09-02 09:41:57 +0200

    token salt fix

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   apiToken: {
-    salt: env("ADMIN_TOKEN_SALT", "1c30d94918ec7e7589ebe754c8b2d51a"),
+    salt: env("ADMIN_TOKEN_SALT", crypto.randomBytes(16).toString("base64")),
   },
   auth: {
     secret: env("ADMIN_JWT_SECRET", "b4dbad35-6caa-4f84-be56-90d0d3bc2fdf"),
