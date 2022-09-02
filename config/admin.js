07be01ecf9fed1bc03835768040603d421910e09
Alex Robert <div@alexrobert.com>
2022-09-02 09:21:43 +0200

    added token salt

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env("ADMIN_TOKEN_SALT", "1c30d94918ec7e7589ebe754c8b2d51a"),
+  },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
+    secret: env("ADMIN_JWT_SECRET", "b4dbad35-6caa-4f84-be56-90d0d3bc2fdf"),
   },
 });
